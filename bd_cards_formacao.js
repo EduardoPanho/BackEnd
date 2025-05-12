365705af7bad3d125253e135089c27b8f6dc2e2e
Eduardo Panho - 에드워드 <eduardopanho9@gmail.com>
2025-05-12 17:19:41 -0300

    back end

---
+++ bd_cards_formacao.js
@@ -0,0 +1,10 @@
+const bd_cards_formacao = [
+    { titulo: "Yonsei University", preco: "연세대학교", imagem: "./../assets/Yonsei_Img_1.png" },
+    { titulo: "Formação - Exatas", preco: "Carga horária - 250h", imagem: "./../assets/Yonsei_Img_2.png" },
+    { titulo: "KAIST University", preco: "카이스트대학교", imagem: "./../assets/KAIST_Img_1.png" },
+    { titulo: "Formação - Linguagens - Coreano", preco: "Carga horária - 130h", imagem: "./../assets/KAIST_Img_2.png" },
+    { titulo: "Sejong University", preco: "세종대학교", imagem: "./../assets/Sejong_Img_1.png" },
+    { titulo: "Formação - Sistemas", preco: "Carga horária - 120h", imagem: "./../assets/Sejong_Img_2.png" },
+];
+
+module.exports = bd_cards_formacao;
